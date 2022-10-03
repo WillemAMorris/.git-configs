4d18f4a4ee74f473bb14b4ddefad2521eb181075
WillemAMorris <wamorris278@gmail.com>
2022-10-02 23:25:56 -0400

    added awesome_fork

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Package Configs
+
+Add awesome config
---
+++ awesome_fork/configuration/client/keys.lua
@@ -0,0 +1,38 @@
+local awful = require('awful')
+require('awful.autofocus')
+local modkey = require('configuration.keys.mod').modKey
+local altkey = require('configuration.keys.mod').altKey
+
+local clientKeys =
+  awful.util.table.join(
+  awful.key({modkey}, 'f',
+    function(c)
+      c.fullscreen = not c.fullscreen
+      c:raise()
+    end,
+    {description = 'toggle fullscreen', group = 'client'}
+  ),
+  awful.key({modkey}, "m",
+    function (c)
+      c.maximized = not c.maximized
+      c:raise()
+    end,
+    {description = 'toggle maximize', group = 'client'}
+  ),
+  awful.key({modkey}, 'n', 
+    function(c) 
+      c.minimized = true 
+    end, 
+    {description = 'minimize', group = 'client'}
+  ),
+  awful.key(
+    {modkey},
+    'q',
+    function(c)
+      c:kill()
+    end,
+    {description = 'close', group = 'client'}
+  )
+)
+
+return clientKeys
